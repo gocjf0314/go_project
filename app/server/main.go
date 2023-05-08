ab1e5a502e4ef25feb3e9879f9630c0e752129e9
Hae Cheol Jeong <40590800+gocjf0314@users.noreply.github.com>
2023-05-08 18:01:01 +0900

    Basic set main func

--- app/server/main.go
+++ app/server/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const portNumber = "9000"
+
+/*
+Network: tcp
+Address: portNumber
+*/
+
+func main() {
+	lis, err := net.Listen("tcp", ":"+portNumber)
+	if err != nil {
+		// TODO: Handle error...
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// Create new Server
+	grpcServer := grpc.NewServer()
+
+	log.Printf("start gRPC server on %s port", portNumber)
+	if err := grpcServer.Serve(lis); err != nil {
+		// TODO: Handle error...
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
