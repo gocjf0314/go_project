85cc3f9ddc26504608e581fc4b41ff38a22426a3
Hae Cheol Jeong <40590800+gocjf0314@users.noreply.github.com>
2023-05-26 05:52:49 +0900

    Edit something

--- env/env.go
+++ env/env.go
@@ -2,15 +2,21 @@ package env
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
 	err := godotenv.Load("env/go.env")
-
 	if err != nil {
 		log.Fatal("Error loading .env file")
 		log.Fatal(err)
 	}
+}
+
+func GetClientEnv() (string, string) {
+	host := os.Getenv("LOCALHOST")
+	portNumber := os.Getenv("SERVER_PORT")
+	return host, portNumber
 }
