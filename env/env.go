3bbd3b94d97bd71456430ad367ac97adb66f1aed
Hae Cheol Jeong <40590800+gocjf0314@users.noreply.github.com>
2023-05-29 21:23:58 +0900

    Add GetlistenerEnv method

--- env/env.go
+++ env/env.go
@@ -10,13 +10,18 @@ import (
 func LoadEnv() {
 	err := godotenv.Load("env/go.env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
-		log.Fatal(err)
+		log.Fatalf("Erro[LoadEnv]: %s", err)
 	}
 }
 
-func GetClientEnv() (string, string) {
+func GetServerEnv() (string, string) {
 	host := os.Getenv("LOCALHOST")
 	portNumber := os.Getenv("SERVER_PORT")
+	return host, portNumber
+}
+
+func GetListenerEnv() (string, string) {
+	host := os.Getenv("LOCALHOST")
+	portNumber := os.Getenv("LISTENER_PORT")
 	return host, portNumber
 }
